8b3bf2a98b04e2d1a67124c0ee03d6c167bca99b
Mubo Houston <89697973+MuboHouston@users.noreply.github.com>
2022-08-05 09:27:24 -0500

    inital code, connected server and db and created routes

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config({path: "./config.env"});
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/record"));
+
+//get driver connection
+const dbo = require("./config/connection")
+
+app.listen(PORT, () => {
+    //perform a db connection when server starts
+    dbo.connectToServer(function (err) {
+        if (err) console.log(err)
+    })
+
+    console.log(`Server is running on port: ${PORT}`);
+});
