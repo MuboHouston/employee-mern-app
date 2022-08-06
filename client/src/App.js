6d53aeaf9320ae25952273fc9f655ffc7063e3df
Mubo Houston <89697973+MuboHouston@users.noreply.github.com>
2022-08-05 23:09:51 -0500

    update app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,24 @@
 import React from "react"
 
 //We use Route in order to define the different routes of out application
-// import { Route, Routes } from "react-route-dom";
+import { Route, Routes } from "react-router-dom";
 
 //We import all the components we need in out app
-// import Create from "./components/create";
-// import Edit from "./components/edit"
-// import RecordList from "./components/recordList";
+import Create from "./components/create";
+import Edit from "./components/edit"
+import RecordList from "./components/recordList";
 import Navbar from "./components/navbar";
 
 const App = () => {
     return (
         <div>
             <Navbar />
+
+            <Routes>
+                <Route exact path="/" element={<RecordList />} />
+                <Route path="/edit/:id" element={<Edit />} />
+                <Route path="/create" element={<Create />} />
+            </Routes>
         </div>
     )
 }
