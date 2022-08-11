d891fe5af86d4cd167c36e98164d1487ad521109
Mubo Houston <89697973+MuboHouston@users.noreply.github.com>
2022-08-10 19:48:36 -0500

    push to heroku

--- client/src/components/geolocation.js
+++ client/src/components/geolocation.js
@@ -8,16 +8,16 @@ export default function Geolocation() {
         <div className="geolocation">
             <h3>Geolocation</h3>
             <br />
-            <h5 className='m-4'>Your Latitude: 
+           
                 {
                     location.loaded ? JSON.stringify(location.coordination.lat) : "Location data not available."
                 }
-            </h5>
-            <h5 className='m-4'>Your Longitude:  
+         
+        
                 {
                     location.loaded ? JSON.stringify(location.coordination.lng) : "Location data not available."
                 }
-            </h5>
+         
         </div>
     )
 }
